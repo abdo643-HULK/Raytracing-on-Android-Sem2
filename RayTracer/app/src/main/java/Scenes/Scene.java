d354974e669d95bafbc130474769a368bbc2005f
Abd El Rahaman Shehata <abdo.shehata1504@gmail.com>
2021-06-05 22:55:34 +0200

    Scene Angepasst

--- RayTracer/app/src/main/java/Scenes/Scene.java
+++ RayTracer/app/src/main/java/Scenes/Scene.java
@@ -2,16 +2,29 @@ package Scenes;
 
 import android.content.Context;
 
+import Util.Direction;
+
 /**
  * Created by Andreas on 11.05.2020.
  */
 
 public interface Scene {
     void onSurfaceCreated(Context context);
+
     void onSurfaceChanged(int width, int height);
+
     void onDrawFrame();
+
     void handleTouchPress(float normalizedX, float normalizedY);
-    void handleTouchDrag(float normalizedX, float normalizedY, float normalizedX1, float normalizedY1);
+
+    //    void handleTouchDrag(float normalizedX, float normalizedY, float normalizedX1, float normalizedY1);
+//    void handleTouchDrag(float normalizedX, int normalizedY, int normalizedX1);
+    void handleTouchDrag(Direction direction);
+
     void handleTouchRelease(float normalizedX, float normalizedY);
+
+    void handleScale(float scaleFactor);
+    void handleRotation(float angle, int x, int y);
+
     void onReload();
 }
